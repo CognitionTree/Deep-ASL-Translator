2cddda25ff51753f0feecddcd58175904407a662
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-27 00:03:42 -0400

    Basic implementation of Frame #4

---
+++ Python-Implementation/frame.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+class Frame:
+	def __init__(self, path):
+		self.path = path
+		self.frame = cv2.imread(path)
+	
+	def get_path(self):
+		return self.path
+	
+	def get_frame(self):
+		return self.frame
+	
+	def set_path(self, path):
+		self.path = path
+	
+	def set_frame(self, frame):
+		self.frame = frame
+	
+	def __str__(self):
+		return self.path+'\n'+str(self.frame)
