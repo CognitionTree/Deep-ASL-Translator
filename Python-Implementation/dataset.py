abbae81b776e7ddc2bad0a52e497ef0a596486ca
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-28 14:29:31 -0400

    Main structure for created for the dataset class#6

--- Python-Implementation/dataset.py
+++ Python-Implementation/dataset.py
@@ -1,3 +1,6 @@
+from sign import *
+import glob
+
 class Dataset(object):
 	FRONT_VIEW = 'Font'
 	FACE_VIEW = 'Face'
@@ -11,4 +14,32 @@ class Dataset(object):
 		self.read_signs()
 	
 	def read_signs(self):
-		print 'Implement This'
+		sings_paths = glob.glob(path+'/*')
+		for sign_path in sings_paths:
+			sign = Sign(sign_path)
+			self.signs.append(sign)
+			self.gloss.append(sign.get_gloss())
+	
+	def get_path(self):
+		return self.path
+	
+	def get_view_point(self):
+		return self.view_point
+	
+	def get_glosses(self):
+		return self.glosses
+	
+	def get_gloss_at(self, pos):
+		return self.gloss[pos]
+	
+	def get_signs(self):
+		return self.signs
+	
+	def get_sign_at(self, pos):
+		return self.signs[pos]
+	
+	def get_signs_matrix(self):
+		matrix = []
+		#TODO: Read everithing with this structure [sign1[frame1[],frame2[]], sign2[f1[],f2[]]]
+			
+				
