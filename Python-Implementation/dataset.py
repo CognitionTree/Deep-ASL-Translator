d5d8dffaeab1c7dd96e998108c3e71a9071b60b5
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-28 17:37:48 -0400

    Adding extra functionalities to the Dataset class#6
    1. Creates a sign matrix out of all sign classes
    2. Shuffles the dataset
    3. Creates a dictionary that organizes the data by mapping each gloss
    to all signs performing that gloss
    4. Splits dataset in training and testing

--- Python-Implementation/dataset.py
+++ Python-Implementation/dataset.py
@@ -27,7 +27,7 @@ class Dataset(object):
 		return self.view_point
 	
 	def get_glosses(self):
-		return self.glosses
+		return array(self.glosses)
 	
 	def get_gloss_at(self, pos):
 		return self.gloss[pos]
@@ -40,6 +40,67 @@ class Dataset(object):
 	
 	def get_signs_matrix(self):
 		matrix = []
-		#TODO: Read everithing with this structure [sign1[frame1[],frame2[]], sign2[f1[],f2[]]]
+		for sign in self.signs:
+			sign_matrix = []
+			for frame in sign.get_frames():
+				sign_matrix.append(frame)
+			matrix.append(array(sign_matrix))
+		return array(matrix)
+	
+	def shuffle_dataset(self):
+		shuffle(self.signs)
+		self.glosses = []
+		
+		for sign in self.signs:
+			self.glosses.append(sign.get_gloss())
+	
+	def organize_signs_by_gloss(self):
+		map_gloss_sign = {}
+		for i in range(len(self.gloss)):
+			cur_gloss = self.gloss[i]
+			cur_sign = self.signs[i]
+			
+			if cur_gloss in map_gloss_sign:
+				map_gloss_sign[gloss].append(cur_sign)
+			else:
+				map_gloss_sign[gloss] = [cur_sign]
+		return map_gloss_sign
+	
+	def get_data_split(self, training_fraction):
+		X_train = []
+		y_train = []
+		
+		X_test = []
+		y_test = []
+	
+		signs_matrix = self.get_signs_matrix()
+		#organize dataset by gloss
+		map_gloss_sign = self.organize_signs_by_gloss()
+		
+		#Setting up initial train fractions to 0
+		train_count = {}
+		for gloss in map_gloss_sign:
+			train_fractions[gloss] = 0.0
+		
+		for i in range(len(self.gloss)):
+			cur_gloss = self.gloss[i]
+			cur_sign = signs_matrix[i]
+			
+			#Training
+			if (train_count[cur_gloss]/(1.0*len(map_gloss_sign[cur_gloss]))) < training_fraction:
+				X_train.append(cur_sign)
+				y_train.append(cur_gloss)
+			else:
+				X_test.append(cur_sign)
+				y_test.append(cur_gloss)
+				
+			train_count[cur_gloss] += 1.0
+		
+		return (array(X_train), array(y_train)), (array(X_test), array(y_test))
+		
+			
+		
+		
+				
 			
 				
