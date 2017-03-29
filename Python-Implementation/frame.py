16f3046d578d19ca560a94d6dd38971d9a814f64
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-29 01:06:30 -0400

    Fixing bugs#4

--- Python-Implementation/frame.py
+++ Python-Implementation/frame.py
@@ -4,7 +4,8 @@ import numpy as np
 class Frame(object):
 	def __init__(self, path):
 		self.path = path
-		self.frame = cv2.imread(path)
+		self.frame = cv2.imread(path, 0)
+		self.frame = cv2.resize(self.frame, (0,0), fx=0.05, fy=0.05)
 	
 	def get_path(self):
 		return self.path
