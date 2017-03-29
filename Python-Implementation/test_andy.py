36003700247cf5b04d20fab5f974b28ec3d4d2ef
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-29 01:06:48 -0400

    Testing Main Classes

--- Python-Implementation/test_andy.py
+++ Python-Implementation/test_andy.py
@@ -1,16 +1,42 @@
+from __future__ import print_function
+from numpy import *
+import random
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, LSTM, GlobalAveragePooling1D
+from keras.layers import Conv2D, MaxPooling2D, TimeDistributed
+from keras import backend as K
+from keras.utils import plot_model
+from keras import metrics
 from dataset import *
 
-ds_path = '/Users/andymartinez/Desktop/Dataset'
+#Model parameters
+kernel_size = (5,5)
+
+ds_path = '/Users/andymartinez/Datasets/ASL/Frames'
 
 d = Dataset(ds_path)
-print str(d)
+#print str(d)
 
 d.shuffle_dataset()
 (X_train, y_train), (X_test, y_test) = d.get_data_split(0.5)
-print X_train
+'''print X_train.shape
 print '-----------------------------'
-print y_train
+print y_train.shape
 print '**********************************'
-print X_test
+print X_test.shape
 print '-----------------------------'
-print y_test
+print y_test.shape'''
+
+#X_train = X_train.astype('float32')
+#X_test = X_test.astype('float32')
+print(X_train.shape)
+X_train /= 255.0
+X_test /= 255.0
+
+y_train = keras.utils.to_categorical(y_train, d.get_numb_classes())
+y_test = keras.utils.to_categorical(y_test, d.get_numb_classes())
+print(X_train.shape)
+#model = Sequential()
+
+#model.add(TimeDistributed(Conv2D(32, kernel_size=kernel_size, padding='same', activation='relu', input_shape=input_shape_conv), input_shape=input_shape_time_dist))
