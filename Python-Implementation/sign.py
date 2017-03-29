28cb38b735306b2634054614b906c0789797e716
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-28 22:14:41 -0400

    Fixing bugs#5

--- Python-Implementation/sign.py
+++ Python-Implementation/sign.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import glob
 from frame import *
+from numpy import *
 
 class Sign(object):
 	def __init__(self, path):
 		self.path = path
 		
 		#Getting gloss
-		splitted_path = pat.split('/')
+		splitted_path = path.split('/')
 		self.gloss = splitted_path[len(splitted_path)-1]
 		self.frames = []
 		self.read_frames()
@@ -24,10 +25,19 @@ class Sign(object):
 		return self.path
 	
 	def get_frames(self):
-		return self.frames
+		return array(self.frames)
+
+	def get_frames_matrices(self):
+		frame_matrices = []
+		for frame in self.frames:
+			frame_matrices.append(frame.get_frame())
+		return array(frame_matrices)
 	
 	def get_frame_at(self, pos):
 		return self.frames[pos]
+	
+	def get_frame_matrix_at(self, pos):
+		return self.frames[pos].get_frame()
 	
 	def get_gloss(self):
 		return self.gloss
@@ -45,7 +55,7 @@ class Sign(object):
 		self.gloss = gloss
 	
 	def __str__(self):
-		return self.gloss+ + '\n' +self.path + '\n' + str(len(self.frames))+' frames'
+		return self.gloss + '\n' +self.path + '\n' + str(len(self.frames))+' frames'
 	
 	def __len__(self):
 		return len(self.frames)
