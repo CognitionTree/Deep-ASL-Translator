dedf5f61e5cc353d0ca46a9a9ff8822b56e931d3
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2017-03-28 22:15:10 -0400

    Testing Dataset, Sign, and Frame

--- Python-Implementation/test_andy.py
+++ Python-Implementation/test_andy.py
@@ -1,1 +1,16 @@
+from dataset import *
 
+ds_path = '/Users/andymartinez/Desktop/Dataset'
+
+d = Dataset(ds_path)
+print str(d)
+
+d.shuffle_dataset()
+(X_train, y_train), (X_test, y_test) = d.get_data_split(0.5)
+print X_train
+print '-----------------------------'
+print y_train
+print '**********************************'
+print X_test
+print '-----------------------------'
+print y_test
