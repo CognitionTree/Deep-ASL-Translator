154cf15b669ff072fb17caef44f172982af6d175
dflorit <danielaflorit@knights.ucf.edu>
2017-03-31 11:46:27 -0400

    refactoring Conv model for OF and adding some results

--- Python-Implementation/conv_of/run.py
+++ Python-Implementation/conv_of/run.py
@@ -10,29 +10,35 @@ from dataset import *
 from numpy import *
 from plotting_tools import *
 
-dataset_path = '/home/andy/Documents/Datasets/ASL/Optical_flow'
+#Split ratios
 train_frac = 0.75
 val_frac = 0.05
 test_frac = 0.2
 
+#Dataset path  --> Need to modify this
+dataset_path = '/Users/danielaflorit/Github/ASL_Dataset/Optical_flow'
+
+#Getting train, validation and test data
 dataset = Dataset(dataset_path)
 ((X_train, y_train),(X_val, y_val),(X_test, y_test)) = dataset.get_data_split(train_frac, val_frac, test_frac)
 
-batch_size = 10
+#Model parameters
 num_classes = dataset.get_numb_classes()
-epochs = 150
-data_augmentation = True
+n_epochs = 1
+batch_size = 10
+pool_size = (2, 2)
+kernel_size = (3,3)
 
+#Names:
 model_name = 'conv_model.png'
-cm_name = 'Confusion Matrix OF Conv'
+cm_name = 'Confusion Matrix Of Experiment Conv model with Optical Flow'
 
-print("Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 print(X_train.shape, 'train samples')
 print(X_val.shape, 'val samples')
 print(X_test.shape, 'test samples')
+
 numb_images, img_rows, img_cols, img_channels  = X_train.shape
 input_shape = (img_rows, img_cols, img_channels)
-#img_channels = 3
 
 X_train = X_train.astype('float32')
 X_val = X_val.astype('float32')
@@ -46,20 +52,22 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_val = keras.utils.to_categorical(y_val, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
+
+#Building Model:
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding='same',input_shape=input_shape))
+model.add(Conv2D(32, kernel_size, padding='same',input_shape=input_shape))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(32, kernel_size))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=pool_size))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(64, kernel_size, padding='same'))
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(64, kernel_size))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=pool_size))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
@@ -68,17 +76,36 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
-#model.summary()
+
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-plot_model(model, to_file=model_name, show_shapes=True, show_layer_names=True)
 
-model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_val, y_val), shuffle=True)
+print("============================ Model Parameters ============================")
+print("Kernel Size: ", kernel_size)
+print("Pooling size: ", pool_size)
+print("Number of epochs: ", n_epochs)
+print("Batch size: ", batch_size)
+print("Number of classes: ", num_classes)
 
-score = model.evaluate(X_test, y_test, verbose=0)
+print("============================== Model Summary ==============================")
+model.summary()
 
+print("================================ Training: ================================")
+#Train model
+print("Training:")
+model.fit(X_train, y_train, batch_size=batch_size, epochs=n_epochs, verbose=1, validation_data=(X_val, y_val), shuffle=True)
+
+#Test model
+print("=========================== Evaluating Model: ===========================")
+score = model.evaluate(X_test, y_test, verbose=1)
+
+print("=========================== Evaluation Metrics ===========================")
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+#Plotting model
+#plot_model(model, to_file=model_name, show_shapes=True, show_layer_names=True)
+
+print("=========================== Predicting Labels: ===========================")
 #Checking predictions
 predictions = model.predict_classes(X_test)
 #feature_vectors = model.predict(X_test)
@@ -88,6 +115,9 @@ y_test_label = []
 for y in y_test:
 	y_test_label.append(argmax(y))
 
-print(str(dataset.get_gloss_to_numb()))
+print("============================ Confusion Matrix ============================")
+#Building Confusion Matrix
+#print(str(dataset.get_gloss_to_numb()))
 build_confusion_matrix(y_test_label, predictions, range(dataset.get_numb_classes()), title=cm_name)
 #display_feature_vectors(feature_vectors, y_test_label, predictions)
+print("==========================================================================")
